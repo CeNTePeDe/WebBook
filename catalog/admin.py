4c83962220342fc1b4b85e0cf49d780334a5d4d9
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-12 16:13:28 +0300

    created classes BooksInstanceInline, AuthorAdmin, BookAdmin, BookInstanceAdmin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Author, Book, Genre, Language, Status, BookInstance
 
-admin.site.register(Author)
-admin.site.register(Book)
+# admin.site.register(Author, AuthorAdmin)
+#admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Status)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name']
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'genre', 'language', 'display_author']
+    list_filter = ['genre', 'author']
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('book', 'status')
+
