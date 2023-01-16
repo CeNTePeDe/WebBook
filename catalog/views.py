2e5606ccd2b99a7679d8b6348cf627f1e0fb995a
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-16 17:02:04 +0300

    created classes BookDetailView, AuthorListView, func catalog_book

--- catalog/views.py
+++ catalog/views.py
@@ -3,14 +3,31 @@ from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
+
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
     num_authors = Author.objects.count()
     context = {'num_books': num_books,
                'num_instances': num_instances,
+               'num_instances_available': num_instances_available,
                'num_authors': num_authors}
     return render(request, 'catalog\index.html', context)
 
+
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 3
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 4
+
+def catalog_book(request):
+    book_list = Book.objects.all()
+    context = {'book_list': book_list}
+    return render(request, 'catalog\catalog.html', context)
