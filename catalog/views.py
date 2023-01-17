055cfba62f279d7ed67f66c2273f570bb143d4b0
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-17 15:51:50 +0300

    added session

--- catalog/views.py
+++ catalog/views.py
@@ -9,25 +9,28 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact=2).count()
     num_authors = Author.objects.count()
+    # добавим сессии
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     context = {'num_books': num_books,
                'num_instances': num_instances,
                'num_instances_available': num_instances_available,
-               'num_authors': num_authors}
+               'num_authors': num_authors,
+               'num_visits': num_visits,}
     return render(request, 'catalog\index.html', context)
 
 
 class BookListView(generic.ListView):
     model = Book
-    paginate_by = 3
+    template_name = 'catalog/catalog.html'
+    paginate_by = 5
+
 
 class BookDetailView(generic.DetailView):
     model = Book
+    template_name = 'catalog/book_detail.html'
+
 
 class AuthorListView(generic.ListView):
     model = Author
-    paginate_by = 4
-
-def catalog_book(request):
-    book_list = Book.objects.all()
-    context = {'book_list': book_list}
-    return render(request, 'catalog\catalog.html', context)
+    paginate_by = 5
