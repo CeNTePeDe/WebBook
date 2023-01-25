2ef526a6e856880b98dafea6c273bfe8307c17b4
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-25 16:30:20 +0300

    added urls for book-detail, book_create, book_update, book_delete

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import index, LoanedBookByUserListView
+from .views import index, LoanedBookByUserListView, BookCreate, BookDelete,BookUpdate, BookDetailView
 from django.conf.urls import url
 
 urlpatterns = [
     path('', index, name='index'),
-    # url(r'^book/(?P<bookid>\d+)/$', BookDetailView.as_view(), name='book-detail'),
-]
+    ]
 urlpatterns += [
-    url(r'^mybooks/$', LoanedBookByUserListView.as_view(), name='my-borrowed')
+    url(r'^mybooks/$', LoanedBookByUserListView.as_view(), name='my-borrowed'),
+    url(r'^book/(?P<bookid>\d+)/$', BookDetailView.as_view(), name='book-detail'),
+    url(r'book/create/$', BookCreate.as_view(), name='book_create'),
+    url(r'book/update/(?P<pk>\d+)$', BookUpdate.as_view(), name='book_update'),
+    url(r'book/delete/(?P<pk>\d+)$', BookDelete.as_view(), name='book_delete'),
 ]
