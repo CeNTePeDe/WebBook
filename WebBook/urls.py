b9842daf4f79d0b49aea26cf9427a1d6c806bc56
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-25 16:30:53 +0300

    added urls for edit1, create, delete

--- WebBook/urls.py
+++ WebBook/urls.py
@@ -19,17 +19,19 @@ from django.urls import re_path
 from django.conf.urls import url
 
 from catalog.models import Book
-from catalog.views import index, BookListView, BookDetailView, AuthorListView, LoanedBookByUserListView, authors_add
+from catalog.views import index, BookListView, BookDetailView, AuthorListView, LoanedBookByUserListView, authors_add, edit1, delete, create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('catalog.urls')),
     re_path(r'^books/$', BookListView.as_view(), name='books'),
     path('book/<int:book_pk>/', BookDetailView.as_view(), name='book-detail'),
     re_path(r'^authors/$', AuthorListView.as_view(), name='authors'),
-
     re_path(r'^mybooks/$', LoanedBookByUserListView.as_view(), name='my-borrowed'),
-    path('author_add/', authors_add, name='authors_add' ),
+    path('authors_add/', authors_add, name='authors_add'),
+    path('edit1/<int:id>/', edit1, name='edit1'),
+    path('create/', create, name='create'),
+    path('delete/<int:id>/', delete, name='delete'),
 ]
 
 urlpatterns += [
