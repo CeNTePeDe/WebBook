cad677187805435af7f1c248f8b8a6bc8af26cd9
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-01-31 15:29:50 +0300

    add cache

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import index, LoanedBookByUserListView, BookCreate, BookDelete,BookUpdate, BookDetailView
 from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', cache_page(60)(index), name='index'),
     ]
 urlpatterns += [
     url(r'^mybooks/$', LoanedBookByUserListView.as_view(), name='my-borrowed'),
-    url(r'^book/(?P<bookid>\d+)/$', BookDetailView.as_view(), name='book-detail'),
     url(r'book/create/$', BookCreate.as_view(), name='book_create'),
     url(r'book/update/(?P<pk>\d+)$', BookUpdate.as_view(), name='book_update'),
     url(r'book/delete/(?P<pk>\d+)$', BookDelete.as_view(), name='book_delete'),
