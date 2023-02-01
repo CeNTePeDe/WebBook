027236404dc037d6f18725dc210a7eca7960d16f
CeNTePeDe <108580276+CeNTePeDe@users.noreply.github.com>
2023-02-01 10:43:35 +0300

    add captcha path

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import index, LoanedBookByUserListView, BookCreate, BookDelete,BookUpdate, BookDetailView
+
+
+from .views import index, LoanedBookByUserListView, BookCreate, BookDelete, BookUpdate, BookDetailView, ContactFormView
 from django.conf.urls import url
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('', cache_page(60)(index), name='index'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
     ]
 urlpatterns += [
     url(r'^mybooks/$', LoanedBookByUserListView.as_view(), name='my-borrowed'),
